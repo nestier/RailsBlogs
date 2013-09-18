45e5cb4a8cd88e286075574040cb3b1cd7dde42c
fernan <fernan@tuVieja.(none)>
2013-09-18 01:02:07 -0300

    validaciones y retoques

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,20 @@
 class PostsController < ApplicationController
 	def new
-	end
+		@post =Post.new
+	end	
 	def show
 		@post = Post.find(params[:id])
 	end
 	def index
-		@post =Post.all
+		@posts =Post.all
 	end			
 	def create
-		@post = Post.new(post_params)
-
-		if @post.save
-			redirect_to @post
-		else
-			render 'new'
-		end		
+  		@post = Post.new(params[:post].permit(:title, :text))
+ 
+  		if @post.save
+    		redirect_to @post
+  		else
+    		render 'new'
+  		end
 	end
-	private
-		def post_params
-			params.require(:post).permit(:title, :text)
-		end
-end
+  end
