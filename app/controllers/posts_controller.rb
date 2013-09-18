82699c9194c63c9754dc48d98e2d10dd496829e2
fernan <fernan@tuVieja.(none)>
2013-09-18 01:23:16 -0300

    botones edit y show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,15 @@ class PostsController < ApplicationController
 	def new
 		@post =Post.new
 	end	
+
 	def show
 		@post = Post.find(params[:id])
 	end
+
 	def index
 		@posts =Post.all
-	end			
+	end	
+
 	def create
   		@post = Post.new(params[:post].permit(:title, :text))
  
@@ -17,4 +20,19 @@ class PostsController < ApplicationController
     		render 'new'
   		end
 	end
+
+	def edit
+  		@post = Post.find(params[:id])
+	end
+
+	def update
+  		@post = Post.find(params[:id])
+ 
+  		if @post.update(params[:post].permit(:title, :text))
+    		redirect_to @post
+  		else
+    		render 'edit'
+  		end
+	end
+
   end
