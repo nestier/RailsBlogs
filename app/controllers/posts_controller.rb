b22b8bf1569e55248eb6d1df02a7866000849d23
fernan <fernan.nestier@gmail.com>
2013-09-19 17:46:40 -0300

    comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,4 +35,11 @@ class PostsController < ApplicationController
   		end
 	end
 
-  end
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Blog::Application.routes.draw do
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :posts
+  resources :posts do
+      resources :comments
+  end    
   get 'posts', to: 'posts#index'
+
   # You can have the root of your site routed with "root"
   root "welcome#index"
 
