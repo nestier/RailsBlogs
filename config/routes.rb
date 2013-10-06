304409138542e332bff05c67a46830bf77c91ec3
nestier <fernan.nestier@gmail.com>
2013-10-06 15:23:58 -0300

    agregando gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Blog::Application.routes.draw do
+  devise_for :users
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -8,7 +9,7 @@ Blog::Application.routes.draw do
   get 'posts', to: 'posts#index'
 
   # You can have the root of your site routed with "root"
-  root "welcome#index"
+  root "posts#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
